a94be02e1bac3de14d797c051d6bc427de142447
lewist13 <lewistpro@gmail.com>
2023-02-28 19:32:22 -0500

    Initial Commit

---
+++ Flixster Pt. 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster Pt. 1
+//
+//  Created by TizzyMatic on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
