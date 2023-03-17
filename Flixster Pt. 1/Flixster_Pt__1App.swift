43ea2352e6cc8f70570dec6270f5016c8007479e
lewist13 <lewistpro@gmail.com>
2023-03-17 15:26:39 -0400

    Initial Commit

---
+++ Flixster Pt. 1/Flixster_Pt__1App.swift
@@ -0,0 +1,17 @@
+//
+//  Flixster_Pt__1App.swift
+//  Flixster Pt. 1
+//
+//  Created by TizzyMatic on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Flixster_Pt__1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
