38aa4eeb973d3aedca6e7996a4145b97e70fffa2
lewist13 <lewistpro@gmail.com>
2023-03-17 19:55:59 -0400

    added geometry reader for landscape adherence

--- Flixster Pt. 1/DetailMovieView.swift
+++ Flixster Pt. 1/DetailMovieView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct DetailMovieView: View {
-    let movie: [Movie] = Movie.mockMovies
+    let movie: Movie
     
     var body: some View {
-        VStack {
-            ForEach(movie) { movie in
+        GeometryReader { geometry in
+            VStack {
                 VStack {
                     AsyncImage(url: movie.backdropImage) { phase in
                         switch phase {
@@ -22,31 +22,39 @@ struct DetailMovieView: View {
                                 image
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(width: 100, height: 100)
+                                    .frame(width: geometry.size.width)
                             case .failure:
                                 Image(systemName: "photo")
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(width: 100, height: 100)
                             @unknown default:
                                 fatalError()
                         }
                     }
+                    Text(movie.title)
+                        .font(.title2)
                 }
-                HStack {
+                HStack(spacing: 50) {
                     VStack {
                         Text(String(format: "%.1f", movie.voteAverage) + " Vote Average")
                         Text(String(movie.voteCount) + " Vote Count")
                     }
                     Text(String(movie.popularity) + " Popularity")
                 }
+                .padding(.bottom)
+                Text(movie.description)
+                    .font(.body)
             }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+            .background(Color.black)
+            .foregroundColor(.white)
         }
     }
 }
 
 struct DetailMovieView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailMovieView()
+        DetailMovieView(movie:
+            Movie(title: "M3GAN", posterImage: URL(string:posterBase+"/d9nBoowhjiiYc4FBNtQkPY7c11H.jpg")!, description: "A brilliant toy company roboticist uses artificial intelligence to develop M3GAN, a life-like doll programmed to emotionally bond with her newly orphaned niece. But when the doll's programming works too well, she becomes overprotective of her new friend with terrifying results.", backdropImage: URL(string:backdropBase+"/q2fY4kMXKoGv4CQf310MCxpXlRI.jpg")!, voteAverage: 7.5, voteCount: 1822, popularity: 1103.359))
     }
 }
