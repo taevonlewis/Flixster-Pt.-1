c5581bacafdde89668e5191caa4871b3c9b7680d
lewist13 <lewistpro@gmail.com>
2023-03-17 16:13:27 -0400

    added movie model

--- Flixster Pt. 1/MovieView.swift
+++ Flixster Pt. 1/MovieView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    let movie: [Movie] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach(movie) { movie in
+                
+            }
         }
-        .padding()
     }
 }
 
