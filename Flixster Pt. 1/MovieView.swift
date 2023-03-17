87b68e30436c6e84f2d183205d95792efe27c045
lewist13 <lewistpro@gmail.com>
2023-03-17 19:55:14 -0400

    added navigation link and theme to set nav title's color

--- Flixster Pt. 1/MovieView.swift
+++ Flixster Pt. 1/MovieView.swift
@@ -11,46 +11,75 @@ struct ContentView: View {
     let movie: [Movie] = Movie.mockMovies
     
     var body: some View {
-        List {
-            ForEach(movie) { movie in
-                HStack {
-                AsyncImage(url: movie.posterImage) { phase in
-                    switch phase {
-                        case .empty:
-                            ProgressView()
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 100, height: 100)
-                        case .failure:
-                            Image(systemName: "photo")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 100, height: 100)
-                        @unknown default:
-                            fatalError()
+        NavigationView {
+            List {
+                ForEach(movie) { movie in
+                    NavigationLink(destination: DetailMovieView(movie: movie)) {
+                        HStack {
+                            AsyncImage(url: movie.posterImage) { phase in
+                                switch phase {
+                                    case .empty:
+                                        ProgressView()
+                                    case .success(let image):
+                                        image
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 100, height: 100)
+                                    case .failure:
+                                        Image(systemName: "photo")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 100, height: 100)
+                                    @unknown default:
+                                        fatalError()
+                                }
+                            }
+                            VStack(alignment: .leading) {
+                                Text(movie.title)
+                                    .font(.title2)
+                                    .lineLimit(1)
+                                Text(movie.description)
+                                    .font(.body)
+                                    .lineLimit(3)
+                            }
+                            .padding(.init(top: 0, leading: -15, bottom: 0, trailing: 15))
                         }
+                        .foregroundColor(.white)
                     }
-                    VStack(alignment: .leading) {
-                        Text(movie.title)
-                            .font(.title2)
-                            .lineLimit(1)
-                            .foregroundColor(.white)
-                        Text(movie.description)
-                            .font(.body)
-                            .lineLimit(3)
-                            .foregroundColor(.white)
-                    }
-                    .padding(.init(top: 0, leading: -15, bottom: 0, trailing: 15))
                 }
+                .listRowBackground(Color.black)
+                .listRowSeparatorTint(Color.white)
             }
-            .listRowBackground(Color.black)
-            .listRowSeparatorTint(Color.white)
+            .padding(EdgeInsets(top: -10, leading: -20, bottom: -10, trailing: -20))
+            .listStyle(.plain)
+            .background(Color.black)
+            .navigationTitle("Movies")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding(EdgeInsets(top: -10, leading: -20, bottom: -10, trailing: -20))
-        .listStyle(.plain)
-        .background(Color.black)
+    }
+    
+    init(){
+        Theme.navigationBarColors(background: .black, titleColor: .white)
+    }
+}
+
+struct Theme {
+    static func navigationBarColors(background: UIColor = .clear,
+                                    titleColor: UIColor = .black,
+                                    tintColor: UIColor = .black) {
+        
+        let navigationAppearance = UINavigationBarAppearance()
+        navigationAppearance.configureWithOpaqueBackground()
+        navigationAppearance.backgroundColor = background
+        
+        navigationAppearance.titleTextAttributes = [.foregroundColor: titleColor]
+        navigationAppearance.largeTitleTextAttributes = [.foregroundColor: titleColor]
+        
+        UINavigationBar.appearance().standardAppearance = navigationAppearance
+        UINavigationBar.appearance().compactAppearance = navigationAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationAppearance
+        
+        UINavigationBar.appearance().tintColor = tintColor
     }
 }
 
